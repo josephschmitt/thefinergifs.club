dae2320e105fb5c727b20d466ab68a4a97a6940e
Joe Schmitt <joe@compass.com>
2018-04-23 21:19:42 -0400

    Initial app homepage setup

---
+++ src/components/Heading.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="uk-flex uk-flex-left uk-flex-nowrap">
+      <h1>The Finer Gifs Club</h1>
+      <vk-tabs class="tabs" align="right">
+        <vk-tabs-item title="Home"></vk-tabs-item>
+        <vk-tabs-item title="About"></vk-tabs-item>
+      </vk-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {Tabs, TabsItem} from 'vuikit/lib/tabs';
+
+  export default {
+    components: {
+      VkTabs: Tabs,
+      VkTabsItem: TabsItem,
+    }
+  };
+</script>
+
+<style scoped>
+  .tabs {
+    margin-left: auto;
+  }
+</style>
+
---
+++ src/components/SearchField.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="uk-flex uk-flex-center">
+    <form class="uk-search uk-search-large">
+      <span uk-search-icon="" class="uk-search-icon uk-icon">
+        <vk-icon icon="search" ratio="2"></vk-icon>
+      </span>
+      <input class="uk-search-input" type="search" placeholder="Search...">
+    </form>
+  </div>
+</template>
+
+<script>
+  import {IconSearch} from '@vuikit/icons';
+
+  export default {
+    components: {
+      VkIconsSearch: IconSearch
+    },
+    data() {
+      return {
+      };
+    }
+  }
+</script>
+
+<style scoped>
+  .icon {
+    display: inline-block;
+  }
+</style>
+
