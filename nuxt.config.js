130d6e4c79988faf6dedc6f2f64d4192db973d88
Joe Schmitt <joe@compass.com>
2018-05-06 17:38:17 -0400

    Add cheesy description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: 'Nuxt.js project'}
+      {hid: 'description', name: 'description',
+          content: 'The Finer Gifs Club: Find the finest gifs from The Office.'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
