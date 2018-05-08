6d88d31c2f05564b0615b173a28d0a49045ef2fd
Joe Schmitt <joe@compass.com>
2018-05-07 22:33:59 -0400

    Don't add page query param if on page 1

--- components/SearchPagination.vue
+++ components/SearchPagination.vue
@@ -1,6 +1,7 @@
 <template>
-  <vk-pagination class="uk-margin-large-top" align="center" :page.sync="page" :perPage="perPage"
-      :total="resultsCount" v-show="resultsCount > perPage">
+  <vk-pagination class="uk-margin-large-top" align="center" :page="currentPage || 1"
+      @update:page="updatePage($event)" :perPage="perPage" :total="resultsCount"
+      v-show="resultsCount > perPage">
     <vk-pagination-page-prev label="Previous"></vk-pagination-page-prev>
     <vk-pagination-pages></vk-pagination-pages>
     <vk-pagination-page-next label="Next"></vk-pagination-page-next>
@@ -14,19 +15,22 @@
   export default {
     data() {
       return {
-        page: this.currentPage || 1,
         perPage: ITEMS_PER_PAGE,
       };
+    },
+    methods: {
+      updatePage(page) {
+        const query = Object.assign({}, this.$route.query, {page});
+        if (page === 1) {
+          delete query.page;
+        }
+
+        this.$router.push({name: this.$route.name, query});
+      }
     },
     computed: {
       ...mapState(['currentPage', 'resultsCount']),
     },
-    watch: {
-      page() {
-        const query = Object.assign({}, this.$route.query, {page: this.page});
-        this.$router.push({name: this.$route.name, query});
-      }
-    }
   };
 </script>
 
